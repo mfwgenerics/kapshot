bf398939398daf4bca7f7717d035459758c4d128
Damien O'Hara <damien.j.ohara@gmail.com>
2022-12-19 22:07:21 +0800

    jvm workflow version fix + pluginBundle config

--- kapshot-plugin-gradle/build.gradle.kts
+++ kapshot-plugin-gradle/build.gradle.kts
@@ -6,6 +6,8 @@ plugins {
     id("conventions")
 
     id("java-gradle-plugin")
+    id("com.gradle.plugin-publish") version "1.1.0"
+
     kotlin("jvm") version "1.7.21"
 
     id("com.github.gmazzo.buildconfig") version "3.1.0"
@@ -19,11 +21,18 @@ dependencies {
     implementation(kotlin("gradle-plugin-api"))
 }
 
+pluginBundle {
+    website = "https://github.com/mfwgenerics/kapshot"
+    vcsUrl = "https://github.com/mfwgenerics/kapshot.git"
+    tags = listOf("kotlin", "kapshot", "jvm")
+}
+
 gradlePlugin {
     plugins {
         create("kapshotPlugin") {
             id = "io.koalaql.kapshot-plugin"
             displayName = "Kapshot Plugin"
+            description = "Kotlin Compiler Plugin for source capture in closure blocks"
             implementationClass = "io.koalaql.kapshot.GradlePlugin"
         }
     }
