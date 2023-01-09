c1e017f04b6a79173fe34e4c4b3c1a8cc99bc951
Damien O'Hara <damien.j.ohara@gmail.com>
2023-01-09 11:15:23 +0800

    empty block / whitespace block tests

--- testing/src/test/kotlin/CapturedLocationTests.kt
+++ testing/src/test/kotlin/CapturedLocationTests.kt
@@ -17,4 +17,32 @@ class CapturedLocationTests {
         assertEquals(12, location.from.column)
         assertEquals(8, location.from.line)
     }
+
+    @Test
+    fun `empty block`() {
+        val block = CapturedBlock {
+
+        }
+
+        val location = block.source().location
+
+        assertEquals("", block.source().text)
+        assertEquals("src/test/kotlin/CapturedLocationTests.kt", location.path)
+        assertEquals(634, location.from.char)
+        assertEquals(35, location.from.column)
+        assertEquals(22, location.from.line)
+    }
+
+    @Test
+    fun `really empty block`() {
+        val block = CapturedBlock {}
+
+        val location = block.source().location
+
+        assertEquals("", block.source().text)
+        assertEquals("src/test/kotlin/CapturedLocationTests.kt", location.path)
+        assertEquals(1044, location.from.char)
+        assertEquals(35, location.from.column)
+        assertEquals(37, location.from.line)
+    }
 }
