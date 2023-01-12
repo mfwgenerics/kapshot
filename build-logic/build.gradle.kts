59c0a82389328f68cd0c532ee2f88d8758e4f12f
Damien O'Hara <damien.j.ohara@gmail.com>
2023-01-12 11:14:35 +0800

    common build logic into conventions plugin + 1.8 target

--- build-logic/build.gradle.kts
+++ build-logic/build.gradle.kts
@@ -7,5 +7,6 @@ repositories {
 }
 
 dependencies {
+    implementation("org.jetbrains.kotlin:kotlin-gradle-plugin:1.8.0")
     implementation("com.palantir.gradle.gitversion:gradle-git-version:0.15.0")
 }
--- build-logic/src/main/kotlin/conventions.gradle.kts
+++ build-logic/src/main/kotlin/conventions.gradle.kts
@@ -1,4 +1,12 @@
+import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+
+repositories {
+    mavenCentral()
+}
+
 plugins {
+    kotlin("jvm")
+
     id("com.palantir.git-version")
 }
 
@@ -9,3 +17,13 @@ version = gitVersion()
 
 check("$version".isNotBlank() && version != "unspecified")
     { "invalid version $version" }
+
+java {
+    targetCompatibility = JavaVersion.VERSION_1_8
+}
+
+tasks.withType<KotlinCompile>().configureEach {
+    kotlinOptions {
+        jvmTarget = "1.8"
+    }
+}
