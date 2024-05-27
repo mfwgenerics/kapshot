fd54093dccd4e090dcaab2c824631be40b496aef
Damien O'Hara <damo@millpointlabs.com>
2024-05-27 18:32:02 +0800

    remove kapt and register services manually

--- kapshot-plugin-kotlin/src/main/kotlin/io/koalaql/kapshot/plugin/Registrar.kt
+++ kapshot-plugin-kotlin/src/main/kotlin/io/koalaql/kapshot/plugin/Registrar.kt
@@ -1,6 +1,5 @@
 package io.koalaql.kapshot.plugin
 
-import com.google.auto.service.AutoService
 import org.jetbrains.kotlin.backend.common.extensions.IrGenerationExtension
 import org.jetbrains.kotlin.cli.common.CLIConfigurationKeys
 import org.jetbrains.kotlin.compiler.plugin.CompilerPluginRegistrar
@@ -8,7 +7,6 @@ import org.jetbrains.kotlin.config.CompilerConfiguration
 import org.jetbrains.kotlin.compiler.plugin.ExperimentalCompilerApi
 import kotlin.io.path.Path
 
-@AutoService(CompilerPluginRegistrar::class)
 @OptIn(ExperimentalCompilerApi::class)
 class Registrar: CompilerPluginRegistrar() {
     override val supportsK2: Boolean get() = true
